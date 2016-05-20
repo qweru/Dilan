42b263fb9c6ab1d4e4bd49e1d961773bd5b290b8
qweru <sara01dilan@gmail.com>
2016-05-20 15:03:46 +0430

    Update write.lua

--- plugins/write.lua
+++ plugins/write.lua
@@ -106,7 +106,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."______________________________\nrMoonTeam"
+	return result_text.."______________________________"
 end
 
 return {
