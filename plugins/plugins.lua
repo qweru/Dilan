35b9e65165d62ba1a0c14dee1f56d144eaa678fd
qweru <sara01dilan@gmail.com>
2016-06-01 15:02:38 +0430

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -173,7 +173,7 @@ end
 
 return {
   patterns = {
-    "^#لیست$",
+    "^#plugins$",
     "^#plugins? (enable) ([%w_%.%-]+)$",
     "^#plugins? (disable) ([%w_%.%-]+)$",
     "^#plugins? (enable) ([%w_%.%-]+) (gp)",
