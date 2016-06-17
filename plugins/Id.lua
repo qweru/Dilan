aba4fe30dcfd0b63946d52c9636dfd9947d351ad
qweru <sara01dilan@gmail.com>
2016-06-17 10:36:50 +0430

    Update Id.lua

--- plugins/Id.lua
+++ plugins/Id.lua
@@ -1,6 +1,6 @@
 do
 function run(msg, matches)
-  return " 🔹 #Group id : "..msg.to.id.."\n🔺 #Group Name : "..msg.to.title.."\n🔹 #Your name : "..(C or '').."\n🔸 #First name : "..(msg.from.first_name or '').."\n🔹 #Last name : "..(msg.from.last_name or '').."\n🔸 #Id : "..msg.from.id.."\n🆔 #Username : @"..(msg.from.username or '').."\n📱 #Phone Number : +"..(msg.from.phone or '').."\nℹ️#User interface :---".."\n⚡️ #IP:....".."\n🌐 #Cuntry:iran".."\n🔃 #:".."\n🔹Telegram link # Telegram.me/"..(msg.from.username or '').."\n🔸 :.... ".."\n🔹 #City:...."
+  return "#Group id : "..msg.to.id.."\n🔺 #Group Name : "..msg.to.title.."\n🔹 #Your name : "..(C or '').."\n🔸 #First name : "..(msg.from.first_name or '').."\n🔹 #Last name : "..(msg.from.last_name or '').."\n🔸 #Id : "..msg.from.id.."\n🆔 #Username : @"..(msg.from.username or '').."\n📱 #Phone Number : +"..(msg.from.phone or '').."\nℹ️#User interface :---".."\n⚡️ #IP:....".."\n🌐 #Cuntry:iran".."\n🔃 #:".."\n🔹Telegram link # Telegram.me/"..(msg.from.username or '').."\n🔸 :.... ".."\n🔹 #City:...."
 end
 return {
   description = "id", 
@@ -10,4 +10,4 @@ return {
   },
   run = run
 }
-end🔹
+end
