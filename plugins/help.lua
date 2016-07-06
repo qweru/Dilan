5e364f070db5c1407ae0dc4871b47db26e50d5c6
qweru <sara01dilan@gmail.com>
2016-07-06 10:49:31 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -89,7 +89,7 @@ function run(msg, matches)
 
 .......................................
 📡
-- @Dilanam
+- @HellCrazy
 .......................................]]
     if matches[1] == 'help' and is_sudo(msg) then
       send_large_msg("user#id"..msg.from.id, help)      
