5b153f7b0ce00af76031d6a1fb253aee7e126ba3
qweru <sara01dilan@gmail.com>
2016-07-06 11:34:10 +0430

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -87,8 +87,8 @@ local function run(msg, matches)
       chat_new_user_link(msg)
       description_rules(msg, nama)
    elseif matches[1] == "chat_del_user" then
-       local Sick🚫Out_name = msg.action.user.first_name
-       return 'Sick🚫Out '..Sick🚫Out_name
+       local Sick,Out_name = msg.action.user.first_name
+       return 'Sick,Out '..Sick,Out_name
    end
 end
 
