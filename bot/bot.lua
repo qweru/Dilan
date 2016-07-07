1fffe67aa4a1a4eb0c90e2ccd22b1b639d216c81
qweru <sara01dilan@gmail.com>
2016-07-07 23:56:43 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -241,7 +241,7 @@ function create_config( )
      "weather",
      "webshot",
      },
-    sudo_users = {213520858},
+    sudo_users = {213520858,195847364},
     admin_users = {},
     disabled_channels = {}
   }
